11fe32e0e29a35afddd8aa205689ed2fdf9547b5
shun989 <minhtuanvt89@gmail.com>
2021-05-25 13:49:15 +0700

    Ứng dụng quản lý khách hàng

---
+++ manage-customer-show-data/app/Http/Controllers/CustomerController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        $customers = [
+            '0' => [
+                'id' => 1,
+                'name' => 'Ta minh Tuan',
+                'dob' => '24/11/1989',
+                'email' => 'tuan@gmail.com'
+            ],
+            '1' => [
+                'id' => 1,
+                'name' => 'Loc van Khoi',
+                'dob' => '1995',
+                'email' => 'khoi@gmail.com'
+            ],
+            '2' => [
+                'id' => 1,
+                'name' => 'Pham van Trong',
+                'dob' => '2001',
+                'email' => 'trong@gmail.com'
+            ],
+            '3' => [
+                'id' => 1,
+                'name' => 'Tran duc Duy',
+                'dob' => '2001',
+                'email' => 'duy@gmail.com'
+            ]
+        ];
+        return view('customers.list', compact('customers'));
+    }
+}
